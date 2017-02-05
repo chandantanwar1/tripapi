d0bc8c68eb19091eb3de0a5489120922ea338768
chandantanwar1 <ggemini6@gmail.com>
2017-02-05 18:27:23 +0530

    First commit

---
+++ test/Nearbycall.js
@@ -0,0 +1,34 @@
+   var abc;
+   var results;
+    var assert = require("assert");
+
+    var NearBySearch = require("../lib/NearBySearch");
+    var config = require("./config.js");
+
+    var nearBySearch = new NearBySearch(config.apiKey, config.outputFormat);
+
+    var parameters = {
+        location: [28.640446,77.1027291],
+        keyword: "restaurants",
+        radius : "1000",
+        rankby : "prominence"     	
+    };
+ 
+  function sample(callback)
+    {
+    
+ nearBySearch(parameters,  function (error, response) {
+        if (error)
+        	{throw error;
+        	//callback();
+        	}	
+       // assert.notEqual(response.results.length, 0, "Place search must not return 0 results");
+     console.log(response.results[0].types);
+       callback(response);
+    });
+    }
+  
+ //console.log(abc);
+ sample(function(res){
+	    console.log(res);
+	});
